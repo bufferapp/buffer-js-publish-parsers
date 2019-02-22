6cbf92d98eb1f49ac95c218484510bb3f1f2bdee
Maya Uribe <mariale.uribe@gmail.com>
2019-02-22 12:01:49 +0100

    Running prettier to new method.

--- src/postParser.js
+++ src/postParser.js
@@ -95,11 +95,11 @@ const getUser = post => {
 }
 
 const removeDuplicates = (arr, prop) => {
-    let obj = {}
-    return Object.keys(arr.reduce((prev, next) => {
-        if (!obj[next[prop]]) obj[next[prop]] = next
-        return obj
-    }, obj)).map((i) => obj[i])
+  let obj = {}
+  return Object.keys(arr.reduce((prev, next) => {
+    if (!obj[next[prop]]) obj[next[prop]] = next
+    return obj
+  }, obj)).map((i) => obj[i])
 }
 
 module.exports = post => {
